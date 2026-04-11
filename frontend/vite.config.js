c729c9b4fd65371bebcdebab6816269b8bdad0a1
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-04-11 17:13:26 +0600

    ff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from "@tailwindcss/vite";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [tailwindcss(), react()],
-// });
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
@@ -28,8 +17,9 @@ export default defineConfig({
     }),
   ],
 
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: 'all',
-  },
-});
+server: {
+  host: '0.0.0.0',
+  strictPort: true,
+  allowedHosts: 'all'
+}
+})
